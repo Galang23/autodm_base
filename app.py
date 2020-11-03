66e8b1daf64301c15fcc588bf9f5ac8d708c954d
Galang23 <Galang23@users.noreply.github.com>
2020-11-03 17:40:22 +0700

    Update trigger jadi 5 menit

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def start():
             print("Direct message is empty...")
             dms = tw.read_dm()
             if len(dms) is 0:
-                time.sleep(60)
+                time.sleep(3000)
 
 if __name__ == "__main__":
     start()
